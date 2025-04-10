45c89b827a383bae6cd9122900c84680cc5b1c4d
XGIlniorX <gilniorzica@gmail.com>
2025-04-10 11:18:22 -0400

    Add files via upload

---
+++ exercicio3.js
@@ -0,0 +1,8 @@
+const idade = 20;
+
+     if( idade >= 18){
+        console.log("A pessoa è maior de idade");
+     }
+     else if( idade < 18){
+        console.log("A pessoa e menor de idade");
+     }
