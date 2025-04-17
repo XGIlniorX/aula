9f476a391e647173d8efb885bdadf852c4390086
XGIlniorX <gilniorzica@gmail.com>
2025-04-17 11:05:01 -0400

    Add files via upload

---
+++ Java Script/exercicio1.js
@@ -0,0 +1,18 @@
+const num1 = 30
+const num2 = 20
+    let maior
+
+if(num1 > num2){
+    maior = num1
+    console.log("O maior numero e " + num1)
+} 
+
+else if(num1 === num2){
+    console.log("Os numeros sao iguais")
+}
+
+else{
+    maior = num2
+    console.log("O maior numero e " + num2)
+}
+
