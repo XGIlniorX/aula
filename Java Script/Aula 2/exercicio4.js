5538a57ffcbfdfcf43a83a99c1e6608ff516154a
XGIlniorX <gilniorzica@gmail.com>
2025-04-22 09:45:39 -0400

    Add files via upload

---
+++ Java Script/Aula 2/exercicio4.js
@@ -0,0 +1,3 @@
+const celsius = 30
+    let fahrenheit = celsius *  1.8 + 32
+    console.log( celsius + "C è igual a "+fahrenheit)
---
+++ Java Script/Aula 2/exercicio5.js
@@ -0,0 +1,6 @@
+const base = 20
+const altura = 90
+
+    area = base * altura / 2
+
+    console.log("A area do triangulo è "+area)
