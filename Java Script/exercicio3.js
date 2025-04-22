c8bcc895e66683fc492d460391cd543ea4703dcb
XGIlniorX <gilniorzica@gmail.com>
2025-04-22 09:44:50 -0400

    Delete Java Script/exercicio3.js

--- Java Script/exercicio3.js
+++
@@ -1,8 +0,0 @@
-const idade = 20;
-
-     if( idade >= 18){
-        console.log("A pessoa è maior de idade");
-     }
-     else if( idade < 18){
-        console.log("A pessoa e menor de idade");
-     }
